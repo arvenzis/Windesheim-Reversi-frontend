e7759ee2e54472a060328dc5b220a06e7b852a94
Karen Brakband <karen@impres.nl>
2019-04-26 09:24:59 +0200

    Finished gulp build system

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,54 @@
-const { gulp, src, dest, parallel } = require('gulp');
+const gulp = require('gulp');
+const { src, dest, parallel } = require('gulp');
+
+/** EVERYTHING RELATED TO HTML **/
+let html_files = './*.html';
 
 function html() {
-    return src('./**/*.html').pipe(dest('dist'));
+    return src(html_files)
+            .pipe(gulp.dest('./dist'));
 }
 
-
+/** EVERYTHING RELATED TO CSS **/
 const clean_css = require('gulp-clean-css');
 const auto_prefixer = require('gulp-autoprefixer');
 const concat = require('gulp-concat');
 
+let css_files = './assets/css/**/*.css';
+
 function css() {
-   return src('./assets/css/**/*.css')
-       .pipe(clean_css({compatibility: 'ie9'}))
-       .pipe(auto_prefixer('last 2 version', 'safari 5', 'ie 9'))
-       .pipe(concat('style.min.css'))
-       .pipe(gulp.dest('./dist'));
+   return src(css_files)
+           .pipe(clean_css({compatibility: 'ie9'}))
+           .pipe(auto_prefixer('last 2 version', 'safari 5', 'ie 9'))
+           .pipe(concat('style.min.css'))
+           .pipe(gulp.dest('./dist'));
 }
 
-exports.build = parallel(html, css);
+/** EVERYTHING RELATED TO JS **/
+const order = require('gulp-order');
+const babel = require('gulp-babel');
+const uglify = require('gulp-uglifyjs');
+const js_files = [
+    'assets/js/**/*.js',
+];
+
+function js() {
+    return src(js_files)
+        .pipe(order(js_files, { base: './' }))
+        .pipe(concat('app.js'))
+        .pipe(babel({
+            presets: ["@babel/preset-env"]
+        }))
+        .pipe(uglify(js_files))
+        .pipe(gulp.dest('./dist/js'));
+}
+
+// Watch files
+function watch() {
+    gulp.watch(html_files, html);
+    gulp.watch(css_files, css);
+    gulp.watch(js_files, js);
+}
+
+exports.watch = watch;
+exports.build = parallel(html, css, js);
