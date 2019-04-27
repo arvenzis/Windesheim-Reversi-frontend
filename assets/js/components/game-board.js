decbe823ee1eb5e6447206ecdf878722859fe8d2
Karen Brakband <karen@impres.nl>
2019-04-27 07:45:05 +0200

    Detect which grid item on the game board was clicked

--- assets/js/components/game-board.js
+++ assets/js/components/game-board.js
@@ -1,22 +1,26 @@
 SPA.gameBoard = (function() {
     function init() {
-        for(let $i = 1; $i < 65; $i++) {
+        for(let i = 1; i < 65; i++) {
             let gridItem = document.createElement('div');
             gridItem.setAttribute('class', 'grid-item');
             document.getElementById('grid-container').appendChild(gridItem);
 
-            if ($i === 28 || $i === 37) {
+            if (i === 28 || i === 37) {
                 let whiteDisc = document.createElement('div');
                 whiteDisc.setAttribute('class', 'white-disc');
                 $(whiteDisc).appendTo(gridItem);
             }
-            else if ($i === 29 || $i === 36) {
+            else if (i === 29 || i === 36) {
                 let blackDisc = document.createElement('div');
                 blackDisc.setAttribute('class', 'black-disc');
                 $(blackDisc).appendTo(gridItem);
             }
         }
     }
+
+    $("#grid-container").click(function(e) {
+        $(e.target).closest('.grid-item').addClass('clicked');
+    });
 
     return {
         init,
