22405216cfb02f6a6dc035188a50667d6709d02e
Karen Brakband <karen@impres.nl>
2019-05-04 08:17:04 +0200

    Ladies and gentleman: we can now create a new game and retrieve a specified game.

--- assets/js/modules/data.js
+++ assets/js/modules/data.js
@@ -9,22 +9,49 @@ SPA.data = (function() {
         }
     }
 
-    function getGames() {
+    function getGame(id) {
         if (configMap.environment === "production") {
             $.ajax({
-                url: uri + configMap.endpoints,
+                url: uri + configMap.endpoints + id,
                 success: function(game) {
-                    document.getElementById("spa").innerHTML = game.id;
+                    console.log(`Here's the game you asked for: ${game}`)
                 },
                 error: function() {
-                    throw new Error("kapot henk");
+                    throw new Error("Failed to retrieve games");
                 }
             });
         }
     }
 
+    function createGame(playerId, gameBoard) {
+        if (configMap.environment === "production") {
+            gameBoard = JSON.stringify(gameBoard);
+            let data = JSON.stringify({"playerId" : playerId, "gameBoard" : gameBoard});
+
+            $.ajax({
+                url: uri + configMap.endpoints,
+                async: true,
+                type: "POST",
+                contentType: "application/json",
+                data: data,
+                success: function() {
+                    console.log('A new game has been created');
+                },
+                error: function() {
+                    throw new Error("Create game gave an error. Please try again.");
+                }
+            });
+        }
+    }
+
+    function updateGame(){
+
+    }
+
     return {
         init,
-        getGames,
+        getGame,
+        createGame,
+        updateGame
     }
 })();
--- assets/js/spa.js
+++ assets/js/spa.js
@@ -1,9 +1,21 @@
 var SPA = (function() {
     function init(){
-        SPA.data.init('production', 'api/game/1');
+         SPA.data.init('production', 'api/game/');
+
+        let fields = [
+            [null, null, null, null, null, null, null, null],
+            [null, null, null, null, null, null, null, null],
+            [null, null, null, null, null, null, null, null],
+            [null, null, null, Disc.black, Disc.white, null, null, null],
+            [null, null, null, Disc.white, Disc.black, null, null, null],
+            [null, null, null, null, null, null, null, null],
+            [null, null, null, null, null, null, null, null],
+            [null, null, null, null, null, null, null, null],
+        ];
 
         $("body").append("<section id='spa'></section>");
-        SPA.data.getGames();
+        SPA.data.getGame(8);
+        SPA.data.createGame(2, fields);
         SPA.gameBoard.init();
 
         //SPA.popup.show("Karen", "Is heel aardig", AlertType.success);
